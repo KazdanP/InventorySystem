7acadc36ee38215a4e318abcf7b5f52729285191
KazdanP <Luke.kidd@Hotmail.co.uk>
2022-03-07 12:49:40 +0000

    Initial commit

---
+++ src/test/java/com/ii/InventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
