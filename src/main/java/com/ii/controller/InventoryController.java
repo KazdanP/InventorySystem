3d7e223132dd7629efeac904ed220bebcc6d0b27
KazdanP <Luke.kidd@Hotmail.co.uk>
2022-03-08 16:16:48 +0000

    Added ShowAllItems method

--- src/main/java/com/ii/controller/InventoryController.java
+++ src/main/java/com/ii/controller/InventoryController.java
@@ -1,5 +1,6 @@
 package com.ii.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,5 +26,10 @@ public class InventoryController {
 	@GetMapping("/FindItem/{itemId}")
 	public ResponseEntity<InventoryItem> GetItemById(@PathVariable Long itemId) {
 		return new ResponseEntity<InventoryItem>(this.service.GetItemById(itemId), HttpStatus.FOUND);
+	}
+	
+	@GetMapping("ShowAllItems")
+	public ResponseEntity<List<InventoryItem>> ShowAllItems() {
+		return new ResponseEntity<List<InventoryItem>>(this.service.ShowAllItems(), HttpStatus.FOUND);
 	}
 }
