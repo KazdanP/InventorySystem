27652979377d81607d852cdce91c3692c9e1db03
KazdanP <Luke.kidd@Hotmail.co.uk>
2022-03-09 13:11:04 +0000

    Created sql files for tests

---
+++ src/main/resources/inventory-data.sql
@@ -0,0 +1,8 @@
+insert into inventory_item (inv_slots, item_desc, item_name,  item_type, upgradable)
+values (2, 'A sword', 'Legendary Sword', 'Weapon', true);
+
+insert into inventory_item (inv_slots, item_desc, item_name,  item_type, upgradable)
+values (1, 'A bow','Legendary Bow',  'Weapon', true);
+
+insert into inventory_item (inv_slots, item_desc, item_name,  item_type, upgradable)
+values (1, 'A sword', 'Wooden Sword', 'Weapon', false);
