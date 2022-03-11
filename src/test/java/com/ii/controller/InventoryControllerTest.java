e70b2c7abff248f64377f97e43d0d395b71133c9
KazdanP <Luke.kidd@Hotmail.co.uk>
2022-03-11 14:03:43 +0000

    Built delete all service test

--- src/test/java/com/ii/controller/InventoryControllerTest.java
+++ src/test/java/com/ii/controller/InventoryControllerTest.java
@@ -79,4 +79,10 @@ public class InventoryControllerTest {
 		this.mvc.perform(delete("/DropItem/1"))
 		.andExpect(status().isAccepted());
 	}
+	
+	@Test
+	void DeleteAllTest() throws Exception {
+		this.mvc.perform(delete("/DropAllItems"))
+		.andExpect(status().isAccepted());
+	}
 }
